7a2e4994024470728050107a1c291a6923001f6d
USER <USER@DELL>
2016-05-05 16:25:40 -0500

    Correcciones al momento de imprimir y agregar totales

--- views/export_excel_sector_costo.php
+++ views/export_excel_sector_costo.php
@@ -8,14 +8,18 @@ header('Expires: 0');
 header("Content-Type: charset=utf-8");
 
 echo Open('div', array('id' => 'print_censo_diario', 'class' => 'col-md-12', 'style' => 'padding-left:50px;padding-top:50px;'));
-//Cabecera             
+
+//Cabecera  
+echo '<h4 align="center">CONSOLIDADO DE COSTOS PARA CAMARONERA GONZALES</h4>';
+           
 echo Open('table', array('class' => 'table table-striped table-bordered table-hover table-condensed table-responsive', 'id' => "recTable"));
 echo Open('thead');
 echo Open('tr');
-echo tagcontent('th COLSPAN="2"', 'DIRECTOS');
+echo tagcontent('th COLSPAN="2"', '<p>COSTOS DIRECTOS</p>');
 echo Close('tr');
 echo Close('thead');
 echo Open('tbody');
+$suma_dir=0;
 for ($i = 0; $i < count($directos); $i++) {
     echo Open('tr');
     //echo $directos[$i][0];
@@ -24,23 +28,28 @@ for ($i = 0; $i < count($directos); $i++) {
     echo tagcontent('th', '' . $directos[$i][0] . '');
     if (!empty($valor)) {
         echo tagcontent('th', '' . number_format($data_direc[$i][0]->$var, 2, ',', ' ') . '');
+        $suma_dir = $suma_dir + $data_direc[$i][0]->$var;
     } else {
         echo tagcontent('th', '0');
     }
     echo Close('tr');
 }
+echo Open('tr');
+echo tagcontent('th', '<var>Total costos directos</var>');
+echo tagcontent('th', $suma_dir);
+echo Close('tr');
 echo Close('tbody');
 echo Close('table');
 
-echo lineBreak2(2);
+echo lineBreak2(1);
 
 echo Open('table', array('class' => 'table table-striped table-bordered table-hover table-condensed table-responsive', 'style' => 'page-break-before:always !important;', 'id' => "recTable"));
 echo Open('thead');
 echo Open('tr');
-echo tagcontent('th COLSPAN="2"', 'INDIRECTOS');
+echo tagcontent('th COLSPAN="2"', '<p>COSTOS INDIRECTOS</p>');
 echo Close('tr');
 echo Close('thead');
-echo Open('tbody');
+echo Open('tbody');$suma_indir=0;
 for ($i = 0; $i < count($indirectos); $i++) {
     echo Open('tr');
     //echo $directos[$i][0];
@@ -49,12 +58,28 @@ for ($i = 0; $i < count($indirectos); $i++) {
     echo tagcontent('th', '' . $indirectos[$i][0] . '');
     if (!empty($valor)) {
         echo tagcontent('th', '' . number_format($data_indir[$i][0]->$var, 2, ',', ' ') . '');
+        $suma_indir = $suma_indir + $data_indir[$i][0]->$var;
     } else {
         echo tagcontent('th', '0');
     }
     echo Close('tr');
 }
+echo Open('tr');
+echo tagcontent('th', '<var>Total costos indirectos</var>');
+echo tagcontent('th', $suma_indir);
+echo Close('tr');
 echo Close('tbody');
+echo Close('table');
+
+echo lineBreak2(1);
+$total = $suma_dir + $suma_indir;
+echo Open('table', array('class' => 'table table-striped table-bordered table-hover table-condensed table-responsive', 'id' => "recTable"));
+echo Open('thead');
+echo Open('tr');
+echo tagcontent('th', '<var>Total consolodidado: </var>');
+echo tagcontent('th', $total);
+echo Close('tr');
+echo Close('thead');
 echo Close('table');
 
 echo Close('div');
--- views/print_reporte_sector_costo.php
+++ views/print_reporte_sector_costo.php
@@ -1,18 +1,23 @@
 <?php
+
 // se tratar de abrir un div en esta vista y cerrarlo en otra no se si haya como 
 echo tagcontent('button', '<span class="glyphicon glyphicon-print"></span> Imprimir', array('id' => 'printbtn', 'data-target' => 'bloque_print', 'class' => 'btn btn-default'));
 /* METODO PARA EXPORTA A EXCEL */
-echo tagcontent('a', '<span class="glyphicon glyphicon-export"></span> Exportar a Excel', array('href' => base_url('produccion/consolidado/export_to_excel_sector_costo/' . $fecha_ini . '/' . $fecha_fin. '/' . $sector. '/' . $centro_costo), 'method' => 'post', 'target' => '_blank', 'class' => 'btn btn-success btn-sm'));
+echo tagcontent('a', '<span class="glyphicon glyphicon-export"></span> Exportar a Excel', array('href' => base_url('produccion/consolidado/export_to_excel_sector_costo/' . $fecha_ini . '/' . $fecha_fin . '/' . $sector . '/' . $centro_costo), 'method' => 'post', 'target' => '_blank', 'class' => 'btn btn-success btn-sm'));
 /* FIN METODO EXPOR A EXCEL */
-echo Open('div', array('class' => 'col-md-12', 'id' => 'bloque_print', 'style' => 'font-size:12px;font-family:monospace'));
+echo Open('div', array('class' => 'col-md-12', 'id' => 'bloque_print', 'style' => 'font-size:10px;font-family:monospace'));
 
-//Cabecera             
+//Cabecera  
+echo '<h4 align="center">CONSOLIDADO DE COSTOS PARA CAMARONERA GONZALES</h4>';
+           
 echo Open('table', array('class' => 'table table-striped table-bordered table-hover table-condensed table-responsive', 'id' => "recTable"));
 echo Open('thead');
 echo Open('tr');
-echo tagcontent('th COLSPAN="2"', 'DIRECTOS');
-echo Close('tr');echo Close('thead');
+echo tagcontent('th COLSPAN="2"', '<p>COSTOS DIRECTOS</p>');
+echo Close('tr');
+echo Close('thead');
 echo Open('tbody');
+$suma_dir=0;
 for ($i = 0; $i < count($directos); $i++) {
     echo Open('tr');
     //echo $directos[$i][0];
@@ -21,21 +26,26 @@ for ($i = 0; $i < count($directos); $i++) {
     echo tagcontent('th', '' . $directos[$i][0] . '');
     if (!empty($valor)) {
         echo tagcontent('th', '' . number_format($data_direc[$i][0]->$var, 2, ',', ' ') . '');
+        $suma_dir = $suma_dir + $data_direc[$i][0]->$var;
     } else {
         echo tagcontent('th', '0');
     }
     echo Close('tr');
 }
+echo Open('tr');
+echo tagcontent('th', '<var>Total costos directos</var>');
+echo tagcontent('th', $suma_dir);
+echo Close('tr');
 echo Close('tbody');
+echo Close('table');
 
-echo lineBreak2(2);
-             
-echo Open('table', array('class' => 'table table-striped table-bordered table-hover table-condensed table-responsive','style'=>'page-break-before:always !important;','id' => "recTable"));
+echo Open('table', array('class' => 'table table-striped table-bordered table-hover table-condensed table-responsive', 'style' => 'page-break-before:always !important;', 'id' => "recTable"));
 echo Open('thead');
 echo Open('tr');
-echo tagcontent('th COLSPAN="2"', 'INDIRECTOS');
-echo Close('tr');echo Close('thead');
-echo Open('tbody');
+echo tagcontent('th COLSPAN="2"', '<p>COSTOS INDIRECTOS</p>');
+echo Close('tr');
+echo Close('thead');
+echo Open('tbody');$suma_indir=0;
 for ($i = 0; $i < count($indirectos); $i++) {
     echo Open('tr');
     //echo $directos[$i][0];
@@ -44,12 +54,28 @@ for ($i = 0; $i < count($indirectos); $i++) {
     echo tagcontent('th', '' . $indirectos[$i][0] . '');
     if (!empty($valor)) {
         echo tagcontent('th', '' . number_format($data_indir[$i][0]->$var, 2, ',', ' ') . '');
+        $suma_indir = $suma_indir + $data_indir[$i][0]->$var;
     } else {
         echo tagcontent('th', '0');
     }
     echo Close('tr');
 }
+echo Open('tr');
+echo tagcontent('th', '<var>Total costos indirectos</var>');
+echo tagcontent('th', $suma_indir);
+echo Close('tr');
 echo Close('tbody');
+echo Close('table');
+
+$total = $suma_dir + $suma_indir;
+echo Open('table', array('class' => 'table table-striped table-bordered table-hover table-condensed table-responsive', 'id' => "recTable"));
+echo Open('thead');
+echo Open('tr');
+echo tagcontent('th', '<var>Total consolodidado: </var>');
+echo tagcontent('th', $total);
+echo Close('tr');
+echo Close('thead');
+echo Close('table');
 
 echo Close('div');
 ?>
